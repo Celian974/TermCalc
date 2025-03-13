ecf6a670cede46884c09d65410890ee1893ca775
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-13 12:21:57 +0100

    [FIX] fix error message

--- src/add.c
+++ src/add.c
@@ -5,7 +5,9 @@ int add(int n1, int n2)
     long long result = (long long)n1 + (long long)n2;
 
     if (result > INT_MAX || result < INT_MIN) {
-        printf("Invalid number entered.\n");
+        red();
+        printf("Overflow or underflow occured.\n");
+        reset();
         return ERROR_VALUE;
     }
     return (int)result;
