14bc295e3e6486eb1f382e10a584def0fe724810
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-13 11:46:00 +0100

    [FIX] add error handling & overflow checking

--- src/add.c
+++ src/add.c
@@ -1,10 +1,12 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "termcalc.h"
 
 int add(int n1, int n2)
 {
-    int result;
+    long long result = (long long)n1 + (long long)n2;
 
-    result = n1 + n2;
-    return result;
+    if (result > INT_MAX || result < INT_MIN) {
+        printf("Invalid number entered.\n");
+        return ERROR_VALUE;
+    }
+    return (int)result;
 }
