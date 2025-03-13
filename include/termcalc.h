3a122bc0b232f5774ea4f80cf89d982546e0d857
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-13 11:50:59 +0100

    [FIX] move libc include to termcalc.h & add new functions prototypes

--- include/termcalc.h
+++ include/termcalc.h
@@ -1,7 +1,14 @@
 
 #ifndef TERMCALC_H_
     #define TERMCALC_H_
-char *get_operator(char *operator);
+    #define ERROR_VALUE -1
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include <string.h>
+    #include <limits.h>
+    #include "termcolors.h"
+
+char *get_operator();
 int get_first_number();
 int get_second_number();
 int add(int n1, int n2);
