f161c51d8750cf232b683741e2f6b3214669030b
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-13 11:43:09 +0100

    [ADD] terminal color header file

---
+++ include/termcolors.h
@@ -0,0 +1,10 @@
+#ifndef TERMCOLORS_H_
+    #define TERMCOLORS_H_
+void white(void);
+void red(void);
+void blue(void);
+void purple(void);
+void cyan(void);
+void yellow(void);
+void reset(void);
+#endif
