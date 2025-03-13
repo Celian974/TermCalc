4dc55531d78157a284eec53a8f843d63d762ac62
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-13 11:49:19 +0100

    [FIX] improve error handling & fix not freed allocation

--- src/get_operator.c
+++ src/get_operator.c
@@ -1,16 +1,19 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include "termcalc.h"
 
 char *get_operator()
 {
     char *operator = malloc(2 * sizeof(char));
 
+    yellow();
     printf("Enter an operator : ");
+    reset();
     scanf("%1s", operator);
     if (strcmp(operator, "+") != 0 && strcmp(operator, "-") != 0
     && strcmp(operator, "*") != 0 && strcmp(operator, "/") != 0) {
+        red();
         printf("This operator is incorrect.\n");
+        reset();
+        free(operator);
         return NULL;
     }
     return operator;
