cfc31f647125aa09beb654c689aea131c20dfd40
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-13 12:28:03 +0100

    [FIX] add new functions prototype & operation results struct

--- include/termcalc.h
+++ include/termcalc.h
@@ -7,10 +7,18 @@
     #include <string.h>
     #include <limits.h>
     #include "termcolors.h"
+typedef struct {
+    int add_result;
+    int sub_result;
+    int mul_result;
+    int div_result;
+} op_result;
 
 char *get_operator();
 int get_first_number();
 int get_second_number();
 int add(int n1, int n2);
+int substract(int n1, int n2);
+int apply_operation(char *operator, int n1, int n2, op_result *result);
 
 #endif
