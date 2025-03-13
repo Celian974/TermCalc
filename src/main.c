954df3be915fede67e231d63f5c70b236332bb01
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-13 11:49:44 +0100

    [FIX] improve error handling

--- src/main.c
+++ src/main.c
@@ -1,6 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "termcalc.h"
 
 int main(void)
@@ -8,17 +5,27 @@ int main(void)
     char *operator = NULL;
     int n1;
     int n2;
+    int addition_result;
 
+    white();
     printf("Welcome to TermCalc !\n");
+    reset();
     while (1) {
-        operator = get_operator(operator);
+        operator = get_operator();
         if (operator == NULL) {
             continue;
         }
         n1 = get_first_number();
+        if (n1 == ERROR_VALUE) continue;
         n2 = get_second_number();
+        if (n2 == ERROR_VALUE) continue;
         if (strcmp(operator, "+") == 0) {
-            printf ("%d + %d = %d\n", n1, n2, add(n1, n2));
+            addition_result = add(n1, n2);
+            if (addition_result != ERROR_VALUE) {
+                white();
+                printf ("%d + %d = %d\n", n1, n2, addition_result);
+                reset();
+            }
         }
         free(operator);
     }
