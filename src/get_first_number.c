59029f2715bdee41755c5994d674b5d3e234ea52
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-13 11:48:15 +0100

    [FIX] improve error handling

--- src/get_first_number.c
+++ src/get_first_number.c
@@ -1,13 +1,18 @@
-#include <stdio.h>
+#include "termcalc.h"
 
 int get_first_number()
 {
     int n1;
 
+    purple();
     printf("Enter your first number : ");
+    reset();
     if (scanf("%d", &n1) != 1) {
+        red();
         printf("This input is incorrect or is not a number.\n");
-        return 84;
+        reset();
+        while (getchar() != '\n');
+        return ERROR_VALUE;
     }
     return n1;
 }
