ec3cdefde1d014c7c0d94a638bc78975e5da33cc
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-13 12:45:11 +0100

    [FIX] fix ERROR_VALUE constant & add new functions prototype

--- include/termcalc.h
+++ include/termcalc.h
@@ -1,12 +1,13 @@
 
 #ifndef TERMCALC_H_
     #define TERMCALC_H_
-    #define ERROR_VALUE -1
     #include <stdlib.h>
     #include <stdio.h>
     #include <string.h>
     #include <limits.h>
     #include "termcolors.h"
+    #define ERROR_VALUE INT_MAX
+
 typedef struct {
     int add_result;
     int sub_result;
@@ -19,6 +20,7 @@ int get_first_number();
 int get_second_number();
 int add(int n1, int n2);
 int substract(int n1, int n2);
+int multiply(int n1, int n2);
 int apply_operation(char *operator, int n1, int n2, op_result *result);
 
 #endif
