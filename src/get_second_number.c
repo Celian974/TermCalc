4a403b59c26ef1aa254dacd652b9d3ad5054d2dd
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-13 11:48:22 +0100

    [FIX] improve error handling

--- src/get_second_number.c
+++ src/get_second_number.c
@@ -1,13 +1,18 @@
-#include <stdio.h>
+#include "termcalc.h"
 
 int get_second_number()
 {
     int n2;
 
+    purple();
     printf("Enter your second number : ");
+    reset();
     if (scanf("%d", &n2) != 1) {
+        red();
         printf("This input is incorrect or is not a number.\n");
-        return 84;
+        reset();
+        while(getchar() != '\n');
+        return ERROR_VALUE;
     }
     return n2;
 }
